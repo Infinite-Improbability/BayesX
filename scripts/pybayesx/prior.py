b709b486ccaf294cb782de42859a8275399a0ea7
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2023-03-14 14:58:36 +1300

    prior types are subclasses now

--- scripts/pybayesx/prior.py
+++ scripts/pybayesx/prior.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import logging
 import numpy as np
+from abc import ABC, abstractmethod
 
 log = logging.getLogger(__name__)
 rng = np.random.default_rng()
@@ -26,9 +27,12 @@ class Property(Enum):
     z = "z_Prior"  # prior on cluster redshift
 
 
-class Prior:
+class Prior(ABC):
     """
     A prior as defined in BayesX.
+
+    This class is abstract and so cannot be directly instantiated.
+    Subclasses must implement the `sample()` method.
 
     :param type_: Prior type as specified in BayesX's readme
     :type type_: int
@@ -69,64 +73,108 @@ class Prior:
         self.param2: float = param2
         self.true_value: float = true_value
 
-    def sample_prior(self) -> float:
+    @abstractmethod
+    def sample(self) -> float:
         """Randomly select a value from prior, using the same methods as in BayesX.
 
         :return: Random value selected from prior.
         :rtype: float
         """
-        value: float
-        if self.type == 0:
-            assert self.param1 == self.param2
-            value = self.param1
-        elif self.type == 1:
-            # Uniform
-            value = rng.uniform(self.param1, self.param2)
-        elif self.type == 2:
-            # LogUniform
-            lx1 = np.log10(self.param1)
-            lx2 = np.log10(self.param2)
-            r = rng.random()
-            value = 10 ** (lx1 + r * (lx2 - lx1))
-        elif self.type == 3:
-            # Gaussian
-            value = rng.normal(self.param1, self.param2)
-        elif self.type == 4:
-            # LogGaussian
-            value = rng.lognormal(self.param1, self.param2)
-        else:
-            raise Exception("Invalid prior type")
-        return value
 
-    def free(self) -> str:
+    def export(self, fixed=False) -> str:
         """Return prior as defined.
 
-        :return: String of prior params formatted as in BayesX infile.
+        :param fixed: Export true value as delta prior, defaults to False
+        :type fixed: bool, optional
+        :raises AttributeError: When attempting to export fixed prior without setting true value
+        :return: String describing prior as used in BAYES-X infile.
         :rtype: str
         """
-        return f"{self.type}    {self.param1}  {self.param2}"
+        if fixed:
+            if self.true_value is None:
+                raise AttributeError(
+                    "Unable to export fixed prior because no true value has been set."
+                )
+            return f"0    {self.true_value}  {self.true_value}"
+        else:
+            return f"{self.type}    {self.param1}  {self.param2}"
 
-    def fixed(self) -> str:
-        """Return true value of prior as fixed prior
 
-        :return: String of fixed prior at true value formatted as in BayesX infile.
-        :rtype: str
-        """
-        if self.true_value is None:
-            raise AttributeError(
-                "Unable to export fixed prior. No true value has been set."
-            )
-        return f"0    {self.true_value}  {self.true_value}"
+class Delta(Prior):
+    def __init__(
+        self,
+        property: Property,
+        value: float,
+        true_value: float = None,
+        randomise: bool = False,
+    ) -> None:
+        super().__init__(property, 0, value, value, true_value, randomise)
+
+    def sample(self) -> float:
+        return self.sample
 
 
 class Uniform(Prior):
     def __init__(
         self,
         property: Property,
-        type_: int,
-        param1: float,
-        param2: float,
+        min: float,
+        max: float,
         true_value: float = None,
         randomise: bool = False,
     ) -> None:
-        super().__init__(property, type_, param1, param2, true_value, randomise)
+        super().__init__(property, 1, min, max, true_value, randomise)
+
+    def sample(self) -> float:
+        return rng.uniform(self.param1, self.param2)
+
+
+class LogUniform(Prior):
+    def __init__(
+        self,
+        property: Property,
+        min: float,
+        max: float,
+        true_value: float = None,
+        randomise: bool = False,
+    ) -> None:
+        super().__init__(property, 2, min, max, true_value, randomise)
+
+    def sample(self) -> float:
+        lx1 = np.log10(self.param1)
+        lx2 = np.log10(self.param2)
+        r = rng.random()
+        return 10 ** (lx1 + r * (lx2 - lx1))
+
+
+class Normal(Prior):
+    def __init__(
+        self,
+        property: Property,
+        mean: float,
+        standard_deviation: float,
+        true_value: float = None,
+        randomise: bool = False,
+    ) -> None:
+        super().__init__(property, 3, mean, standard_deviation, true_value, randomise)
+
+    def sample(self) -> float:
+        return rng.normal(self.param1, self.param2)
+
+
+class LogNormal(Prior):
+    def __init__(
+        self,
+        property: Property,
+        mean: float,
+        width: float,
+        true_value: float = None,
+        randomise: bool = False,
+    ) -> None:
+        super().__init__(property, 4, mean, width, true_value, randomise)
+
+    def sample(self) -> float:
+        return rng.lognormal(self.param1, self.param2)
+
+
+# TODO: Missing joint prior (see readme)
