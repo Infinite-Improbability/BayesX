d86327d0bea4a7970953363a67e7f46ce4a79947
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2023-03-15 14:28:36 +1300

    add specific models

--- scripts/pybayesx/model.py
+++ scripts/pybayesx/model.py
@@ -1,16 +1,15 @@
-from typing import Iterable
-from .priors import Property
 import logging
+from enum import Enum
+from typing import Iterable
+
+from .priors import Property
 
 log = logging.getLogger(__name__)
 
 
 class Model:
     def __init__(
-        self,
-        name: str,
-        num: int,
-        required_priors: Iterable[Property],
+        self, num: int, required_priors: Iterable[Property], name: str = ""
     ) -> None:
         self.name = name
         self.num = num
@@ -21,3 +20,51 @@ class Model:
             if prop not in input_priors:
                 return False
         return True
+
+
+nfw_gnfw = Model(
+    num=1,
+    required_priors=[
+        Property.x,
+        Property.y,
+        Property.M_200,
+        Property.fg_200,
+        Property.a_GNFW,
+        Property.b_GNFW,
+        Property.c_GNFW,
+        Property.c500_GNFW,
+        Property.z,
+    ],
+    name="NFW-GNFW",
+)
+
+nfw_einasto = Model(
+    num=1,
+    required_priors=[
+        Property.x,
+        Property.y,
+        Property.M_200,
+        Property.fg_200,
+        Property.a_GNFW,
+        Property.b_GNFW,
+        Property.c_GNFW,
+        Property.c500_GNFW,
+        Property.alpha_model2,
+        Property.z,
+    ],
+    name="Einasto",
+)
+
+polytropic = Model(
+    num=1,
+    required_priors=[
+        Property.x,
+        Property.y,
+        Property.M_200,
+        Property.gamma0_poly,
+        Property.gammaR_poly,
+        Property.t0_poly,
+        Property.z,
+    ],
+    name="NFW-GNFW",
+)
