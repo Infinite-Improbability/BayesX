5ff69c1d3c40f6c3d8058f9f2d4bc9f4f08e2473
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2023-03-17 16:11:33 +1300

    comments and partial rework of plotting code

--- scripts/pybayesx/plot.py
+++ scripts/pybayesx/plot.py
@@ -11,7 +11,7 @@ log = getLogger(__name__)
 def plot(
     chain_paths: Union[Path, Sequence[Path]],
     parameters: Iterable[Union[int, tuple[int, float] | tuple[int, None]]],
-    output_path: Optional[Path] = None,
+    plot_path: Optional[Path] = None,
 ):
     # Massage input
 
@@ -20,55 +20,63 @@ def plot(
         chain_paths = [chain_paths]
 
     # Set default output path
-    if output_path is None:
-        output_path = Path(chain_paths[0]).joinpath("_tri.png")
+    if plot_path is None:
+        plot_path = Path(chain_paths[0]).joinpath("_tri.png")
 
     # Convert all parameters to tuples of the form (id_number, true_value) using None if true value not provided
     parameters = [p if isinstance(p, tuple) else (p, None) for p in parameters]
 
-    ch: list[MCSamples] = []
+    # Load in chains
+    chains: list[MCSamples] = []
     for p in chain_paths:
-        ch.append(loadMCSamples(str(p)))
+        chains.append(loadMCSamples(str(p)))
 
-    pars = ch[0].getParamNames()
-    if pars is None:
+    # Load parameter definitions for first chain
+    first_chain_pars = chains[0].getParamNames()
+    if first_chain_pars is None:
         raise Exception(
-            f"MCSample {ch[0]} has no parameter names. Is there a .paramnames file alongside the chains?"
+            f"MCSample {chains[0]} has no parameter names. Is there a .paramnames file alongside the chains?"
         )
 
-    plotpars: list[str] = []
+    # Select the parameters we are plotting
+    plotted_pars: list[str] = []
     for p in parameters:
-        plotpars.append(
-            pars.parWithName(
+        plotted_pars.append(
+            first_chain_pars.parWithName(
                 p, error=True
             ).label  # pyright: ignore [reportOptionalMemberAccess]
         )
 
-    plotpar_names = ["plot" + str(i) for i in range(len(plotpars))]
+    # Internal names for the various parameter plots
+    plotpar_names = ["plot" + str(i) for i in range(len(plotted_pars))]
 
-    markers = {}
-    for p, v in parameters:
-        if v is not None:
-            try:
-                markers[p] = float(v)
-            except:
-                print(f"Error applying true value {v} to {p}")
+    # Convert pairs of param id numbers and true values to dict
+    markers = {p: v for p, v in parameters if v is not None}
 
-    for i, samps in enumerate(ch):
+    # Loop over all the chains
+    for i, samps in enumerate(chains):
         parsi = samps.getParamNames()
         if parsi is None:
             raise Exception(
-                f"MCSample {ch[0]} has no parameter names. Is there a .paramnames file alongside the chains?"
+                f"MCSample {chains[0]} has no parameter names. Is there a .paramnames file alongside the chains?"
             )
-        for p_label in plotpars:
+
+        # Search for parameters by name in current chain
+        for p_label in plotted_pars:
             found = False
-            for j, pj in enumerate(parsi.names):
+
+            for pj in parsi.names:
+                # Found parameter
                 if pj.label == p_label:
                     found = True
-                    p_name = plotpar_names[plotpars.index(p_label)]
+
+                    # Get internal name for parameter plot
+                    p_name = plotpar_names[plotted_pars.index(p_label)]
+
+                    # Handle special case parameters
                     if p_label[0] == "M" and "M_{\\odot}" in p_label:
                         # Add a derived parameter for mass to get around labelling issues
-                        M = eval("samps.getParams()." + pj.name)
+                        M = getattr(samps.getParams(), pj.name)
                         M1 = samps.ranges.getLower(pj.name)
                         M2 = samps.ranges.getUpper(pj.name)
                         p_label_new = p_label.replace(
@@ -86,11 +94,11 @@ def plot(
                         else:
                             M2 *= 1e-14
                         samps.setRanges({p_name: [M1, M2]})
-                        if p_label in markers.keys() and i == 0:
+                        if p_label in markers and i == 0:
                             markers[p_label] *= 1e-14
                     elif p_label[0] == "S" and "Jy" in p_label:
                         # Convert source fluxes to mJy
-                        S = eval("samps.getParams()." + pj.name)
+                        S = getattr(samps.getParams(), pj.name)
                         S1 = samps.ranges.getLower(pj.name)
                         S2 = samps.ranges.getUpper(pj.name)
                         samps.addDerived(
@@ -107,39 +115,49 @@ def plot(
                         else:
                             S2 *= 1e3
                         samps.setRanges({p_name: [S1, S2]})
-                        if p_label in markers.keys() and i == 0:
+                        if p_label in markers and i == 0:
                             markers[p_label] *= 1e3
                     else:
                         pj.name = p_name
+
             if not found:
                 print(f"Warning: {p_label} not found in {chain_paths[i]}")
-        samps.setParamNames(parsi)
 
-    leg = []
-    for c in ch:
-        leg.append(c.getName())
+        samps.setParamNames(parsi)  # This looks like it undoes all our changes
 
-    g = plots.get_subplot_plotter(width_inch=8)
-    g.settings.axes_fontsize = 8
-    g.settings.alpha_filled_add = 0.4
-    g.triangle_plot(
-        ch,
+    # Setup for plots
+    legend = []
+    for c in chains:
+        legend.append(c.getName())
+    plotter = plots.get_subplot_plotter(width_inch=8)
+    plotter.settings.axes_fontsize = 8
+    plotter.settings.alpha_filled_add = 0.4
+
+    # Plot all the parameters
+    plotter.triangle_plot(
+        chains,
         plotpar_names,
-        filled_compare=True,
-        legend_labels=leg,
+        legend_labels=legend,
+        filled=True,
         legend_loc="upper right",
     )
 
-    assert g.subplots is not None  # this should always be true
+    # This should always be true after triangle_plot call
+    # but pyright appreciates us confirming it. No harm in checking.
+    assert plotter.subplots is not None
 
-    for ipar, p in enumerate(plotpars):
-        if p in markers.keys():
-            ax = g.subplots[ipar, ipar]
+    # Add true values, if they exist
+    for ipar, p in enumerate(plotted_pars):
+        if p in markers:
+            # Add line for true value
+            ax = plotter.subplots[ipar, ipar]
             ax.axvline(x=markers[p], color="k")
-            for jpar, p2 in enumerate(plotpars[ipar + 1 :]):
-                if p2 in markers.keys():
+            # Add star for true values in corrolation plots
+            for jpar, p2 in enumerate(plotted_pars[ipar + 1 :]):
+                if p2 in markers:
                     jplot = ipar + jpar + 1
-                    ax = g.subplots[jplot, ipar]
+                    ax = plotter.subplots[jplot, ipar]
                     ax.plot(markers[p], markers[p2], "*k")
 
-    g.export(output_path)
+    # Save as image
+    plotter.export(plot_path)
