0c593a84e6bb5e727e8c60432fb48a2d4e60d5a8
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2023-03-21 15:54:35 +1300

    better input handling on bin function

--- scripts/pybayesx/pybayesx/binning.py
+++ scripts/pybayesx/pybayesx/binning.py
@@ -1,35 +1,36 @@
-from collections.abc import Sequence
 from logging import getLogger
 from pathlib import Path
-from typing import Any
+from typing import Any, Optional
 
 import numpy as np
+from numpy.typing import ArrayLike
 
 log = getLogger(__name__)
 
 
 def bin(
-    x: Sequence,
-    y: Sequence,
-    channel: Sequence,
+    x: ArrayLike,
+    y: ArrayLike,
+    channel: ArrayLike,
     n_bins: int,
     cellsize: float,
-    x0: float = None,
-    y0: float = None,
-    n_channels: int = None,
-    outfile: Path = None,
+    x0: Optional[float] = None,
+    y0: Optional[float] = None,
+    n_channels: Optional[int] = None,
+    outfile: Optional[Path] = None,
     mask: bool = False,
 ) -> "np.ndarray[Any, np.dtype[np.float64]]":
     """Bin data given spatial coordinates and channel coordinates of events.
+    This will crop points if they are not within `n_bins/2` bins of the center.
     If `mask = True` then instead bin passed on mask status.
 
-    :param x: Sequence of x coordinates of events
-    :type x: Sequence
-    :param y: Sequence of y coordinates of events, assumed to use the same units as x
-    :type y: Sequence
+    :param x: 1D sequence of x coordinates of events. Should have an entry for every point.
+    :type x: numpy.typing.ArrayLike
+    :param y: Sequence of y coordinates of events, assumed to use the same units and order as x.
+    :type y: numpy.typing.ArrayLike
     :param channel: Sequence of channel of events. If `mask` is True then this value
      should be 1 for all masked spatial coordinates and 0 otherwise.
-    :type channel: Sequence
+    :type channel: numpy.typing.ArrayLike
     :param nbins: The number of bins along each spatial axis
     :type nbins: int
     :param cellsize: The size of a bin, in units matching x and y
@@ -57,28 +58,44 @@ def bin(
     :rtype: np.ndarray[Any, np.dtype[np.float64]]
     """
 
+    # Coerce input
+    x = np.asanyarray(x)
+    y = np.asanyarray(y)
+    channel = np.asanyarray(channel)
+
+    # Do some input checking
+    if len(x) != len(y):
+        raise ValueError("Coordinate lists x and y have different lengths.")
+    if x.ndim != 1 or y.ndim != 1:
+        raise ValueError("x or y array is not one dimensional.")
+
+    # Set no. of channels based on largest channel number present
+    if mask and (n_channels is None or n_channels <= 0):
+        raise ValueError("If binning a mask, max_chan is a required argument.")
+    elif n_channels is None:
+        n_channels = int(np.max(channel))
+
     # If origins are not provided default to centre of data
-    if not x0:
+    if x0 is None:
         x0 = (np.max(x) + np.min(x)) / 2
-    if not y0:
+    if y0 is None:
         y0 = (np.max(y) + np.min(y)) / 2
 
+    # Set relative coordinates
     dx = x - x0
     dy = y - y0
 
-    if mask and (n_channels is None or n_channels <= 0):
-        raise ValueError("If binning a mask, max_chan is a required argument.")
+    # Initalize output array
+    counts = np.zeros((n_bins, n_bins, n_channels))
 
-    if not n_channels:
-        n_channels = int(np.max(channel))
-
-    counts: np.ndarray = np.zeros((n_bins, n_bins, n_channels))
-
+    # Set center coordinates in bin basis
     i0 = n_bins / 2
     j0 = n_bins / 2
 
+    # Loop over every cell
+    # The x and y arrays contain an entry for every point so we don't need a nested iteration over dy
     for k in range(len(dx)):
-        # Get cell coordinates, relative to centre
+        # Get cell coordinates in original basis, relative to centre
         u = dx[k]
         v = dy[k]
 
@@ -90,6 +107,7 @@ def bin(
         if v < 0.0:
             signy = -1
 
+        # Convert coordinates to bin basis
         i = int(np.floor(u / cellsize + signx * 0.5) + i0)
         j = int(np.floor(v / cellsize + signy * 0.5) + j0)
 
