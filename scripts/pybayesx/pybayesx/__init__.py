147042a1b264ee1b7466a02d9f2bbfd0c2b76dba
InfiniteImprobability <coxryan@myvuw.ac.nz>
2023-04-10 13:45:05 +1200

    import everything in top level package namespace

--- scripts/pybayesx/pybayesx/__init__.py
+++ scripts/pybayesx/pybayesx/__init__.py
@@ -1,20 +1,19 @@
 # TODO: Log to stdout
 
-# from .binning import bin
-# from .config import AnalysisConfig
-# from .input import Events, Mask, RMF, ARF, DataConfig, load_all_from_fits
-# from .model import (
-#     Property,
-#     Prior,
-#     UniformPrior,
-#     DeltaPrior,
-#     LogNormalPrior,
-#     NormalPrior,
-#     LogUniformPrior,
-#     Model,
-#     nfw_einasto,
-#     nfw_gnfw,
-#     polytropic,
-# )
-# from .plot import plot
-# from .run import run
+"""A Python wrapper to make using BayesX simpler."""
+from .analysis import Analysis, AnalysisConfig
+from .data import ARF, RMF, DataConfig, Events, Mask, load_all_from_fits
+from .model import (
+    DeltaPrior,
+    LogNormalPrior,
+    LogUniformPrior,
+    Model,
+    NormalPrior,
+    Prior,
+    Property,
+    UniformPrior,
+    nfw_einasto,
+    nfw_gnfw,
+    polytropic,
+)
+from .plot import plot
