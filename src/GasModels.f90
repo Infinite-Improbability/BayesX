442df3c370a9c87458fd5c70dee353e34fc82224
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2023-06-12 17:04:17 +1200

    debug logging

--- src/GasModels.f90
+++ src/GasModels.f90
@@ -307,21 +307,29 @@ CONTAINS
             ! write(*,'(I3,E16.8,E16.8,E16.8,E16.8)') m, r(m), Rhogas(m), T(m),  xrayFluxCoeff(m)
          END DO
 
-         ! open(newunit=io, file='log.txt', position="append", status="old", action="write")
-         ! write(io, *) '--------------------------------'
-         ! write(io, *) 'MT200_DM', MT200_DM
-         ! write(io, *) 'fg200_DM', fg200_DM
-         ! write(io, *) 'a_GNFW', a_GNFW
-         ! write(io, *) 'b_GNFW', b_GNFW
-         ! write(io, *) 'c_GNFW', c_GNFW
-         ! write(io, *) 'c500_GNFW', c500_GNFW
-         ! write(io, *) 'rmin_fraction', rmin_fraction
+         open(newunit=io, file='log.txt', position="append", status="old", action="write")
+         write(io, *) '--------------------------------'
+         write(io, *) 'MT200_DM', MT200_DM
+         write(io, *) 'fg200_DM', fg200_DM
+         write(io, *) 'a_GNFW', a_GNFW
+         write(io, *) 'b_GNFW', b_GNFW
+         write(io, *) 'c_GNFW', c_GNFW
+         write(io, *) 'c500_GNFW', c500_GNFW
+         write(io, *) 'rmin_fraction', rmin_fraction
 
-         ! write(io, *) 'r', r
-         ! write(io, *) 'Rhogas', Rhogas
-         ! write(io, *) 'T', T
-         ! write(io, *) 'xrayFluxCoeff', xrayFluxCoeff
+         write(io, *) 'r', r
+         write(io, *) 'Rhogas', Rhogas
+         write(io, *) 'T', T
+         !write(io, *) 'xrayFluxCoeff', xrayFluxCoeff don't actually need this, it is all in the 2D array
          ! write(io, *) 'X_emiss2D', X_emiss2D
+
+         write(io, *) 'X_emiss2D start'
+         do i = 1, xrayNbin
+            do j = 1, n
+               write (io, *) X_emiss2D(j, i)
+            end do
+         end do
+         write(io, *) '---'
 
 
          ! Clear up some memory
@@ -334,6 +342,7 @@ CONTAINS
             ! Looping over points in radius
             DO m = 1, n
                ! Why are we applying the cooling function coefficent now?
+               ! And are we really taking a log at the right place?
                ! This array will be used by XraySintegrand via Xrayemissfunc1
                logX_emiss1D(m) = phlog10(X_emiss2D(m, i)*xfluxsec2*m2cm*m2cm*m2cm*Mpc2m*Mpc2m*Mpc2m)
             END DO
@@ -370,7 +379,14 @@ CONTAINS
             END DO
          END DO
 
-         write(io, *) 'X_S2D', X_S2D
+         ! write(io, *) 'X_S2D', X_S2D
+         write(io, *) 'X_2SD start'
+         do i = 1, xrayNbin
+            do j = 1, n
+               write (io, *) X_S2D(j, i)
+            end do
+         end do
+         write(io, *) '---'
 
          ! Free up some memory
          DEALLOCATE (X_emiss2D, logX_emiss1D, X_S1D)
@@ -403,6 +419,7 @@ CONTAINS
          xrayCpred = 0d0 ! Predicted counts from source and background
 
          ! Iterate over every pixel (cell) in the observation
+         write(io, *) 'X_Cmap start'
          DO xrayxpix = 1, xraynx
             DO xrayypix = 1, xrayny
 
@@ -440,11 +457,14 @@ CONTAINS
 
                   ! Turn predicted count rate into predicted count by applying exposure time and cell size
                   xrayCmap(i, xrayxpix, xrayypix) = (xrayCmap(i, xrayxpix, xrayypix))*(sexpotime)*(xraycell*xraycell*sec2min*sec2min)
+                  write (io, *) xrayCmap(i, xrayxpix, xrayypix)
                END DO
             END DO
          END DO
+         write(io, *) '---'
 
-         write(io, *) 'xrayCmap', xrayCmap
+         ! write(io, *) 'xrayCmap', xrayCmap
+
 
          ! Free up more memory
          DEALLOCATE (predX_S2D)
@@ -461,6 +481,8 @@ CONTAINS
          END DO
 
          write(io, *) 'xrayCpred', xrayCpred
+
+         close(io)
 
          !Store derived parameters
          aux(k, 1) = D              !angular diameter distance in Mpc
